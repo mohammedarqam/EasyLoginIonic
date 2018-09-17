66aead2711e6a1cb8d8d5c40b34dad66ea4e70d8
mohammedarqam <mohammedarqam97@gmail.com>
2018-09-18 02:50:02 +0530

    Pages generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,50 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { LoginPage } from '../pages/login/login';
+import { SignUpPage } from '../pages/sign-up/sign-up';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import * as firebase from 'firebase';
+
+
+export const firebaseCred = {
+  apiKey: "AIzaSyCtgOPbmMn2PhB307Jztz0wo8Zku1ToRME",
+  authDomain: "easylogin-e5fa3.firebaseapp.com",
+  databaseURL: "https://easylogin-e5fa3.firebaseio.com",
+  projectId: "easylogin-e5fa3",
+  storageBucket: "easylogin-e5fa3.appspot.com",
+  messagingSenderId: "852715724645"
+};
+firebase.initializeApp(firebaseCred);
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DashboardPage,
+    LoginPage,
+    SignUpPage,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseCred),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DashboardPage,
+    LoginPage,
+    SignUpPage,
   ],
   providers: [
     StatusBar,
